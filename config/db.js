af367b0249e03417d833b6a05dff5299d86ca32d
Vishesh-Nagar <nagarvishesh16@gmail.com>
2024-09-26 19:03:09 +0530

    New start

--- config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async()=>{
-    await mongoose.connect(process.env.MONGODB_URI, {
-    dbName: 'Delivery'}).then(()=>console.log("DB connected"))
-    .catch(error => console.log(error))
-    
-}
