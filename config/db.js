10eff825aa03e3b35c5e8705fb4be67f635991c5
Vishesh-Nagar <nagarvishesh16@gmail.com>
2024-09-26 20:35:33 +0530

    First Commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectDB = async () => {
-
-//     await mongoose.connect('mongodb+srv://nagarvishesh16:alphabeetagamma123@delivery.dd6hx.mongodb.net/Food-del').then(() => {
-//         console.log("Connected to MongoDB");
-//     })
-// }
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
